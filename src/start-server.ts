023d1d2e31ea3772c512c26124a5ccf54d1f446c
LiJia <lijia.c@gmail.com>
2024-08-29 10:03:08 +0800

    add hostname arg

--- src/start-server.ts
+++ src/start-server.ts
@@ -6,13 +6,17 @@ import app from './index';
 
 // Extract the port number from the command line arguments
 const defaultPort = 8787;
+const defaultHost = '127.0.0.1';
 const args = process.argv.slice(2);
 const portArg = args.find((arg) => arg.startsWith('--port='));
+const hostArg = args.find((arg) => arg.startsWith('--host='));
 const port = portArg ? parseInt(portArg.split('=')[1]) : defaultPort;
+const host = hostArg ? hostArg.split('=')[1] : defaultHost;
 
 serve({
   fetch: app.fetch,
   port: port,
+  hostname: host,
 });
 
-console.log(`Your AI Gateway is now running on http://localhost:${port} 🚀`);
+console.log(`Your AI Gateway is now running on http://${host}:${port} 🚀`);
