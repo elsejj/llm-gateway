f714d63c2f774b8d38677e5b035ab608c6e6c285
LiJia <lijia.c@gmail.com>
2024-08-30 09:31:55 +0800

    add config PORT and HOST from env

--- src/start-server.ts
+++ src/start-server.ts
@@ -5,12 +5,12 @@ import { serve } from '@hono/node-server';
 import app from './index';
 
 // Extract the port number from the command line arguments
-const defaultPort = 8787;
-const defaultHost = '127.0.0.1';
+const defaultPort = process.env.LLM_GATEWAY_PORT || '8787'
+const defaultHost = process.env.LLM_GATEWAY_HOST || '127.0.0.1';
 const args = process.argv.slice(2);
 const portArg = args.find((arg) => arg.startsWith('--port='));
 const hostArg = args.find((arg) => arg.startsWith('--host='));
-const port = portArg ? parseInt(portArg.split('=')[1]) : defaultPort;
+const port = portArg ? parseInt(portArg.split('=')[1]) : parseInt(defaultPort);
 const host = hostArg ? hostArg.split('=')[1] : defaultHost;
 
 serve({
